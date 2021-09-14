cdd289e2ca5e7178612931e956f346bd650193cf
Linus Mac <90680681+ntijoh-linus-sjunnesson@users.noreply.github.com>
2021-09-14 14:56:31 +0200

    Table creation shit

--- lib/wtf/mix/seed.ex
+++ lib/wtf/mix/seed.ex
@@ -11,20 +11,18 @@ defmodule Mix.Tasks.Seed do
 
   defp drop_tables() do
     IO.puts("Dropping tables..")
-    # Postgrex.query!(DB, "DROP TABLE IF EXISTS fruits", [], pool: DBConnection.ConnectionPool)
-    # Postgrex.query!(DB, "DROP TABLE IF EXISTS users", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS classes", [], pool: DBConnection.ConnectionPool)
   end
 
   defp create_tables() do
     IO.puts("Creating tables..")
-    # Postgrex.query!(DB, "Create TABLE fruits (id SERIAL, name VARCHAR(255) NOT NULL, tastiness INTEGER NOT NULL)", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "Create TABLE classes (id SERIAL, school_name VARCHAR(255) NOT NULL)", [], pool: DBConnection.ConnectionPool)
   end
 
   defp seed_data() do
     IO.puts("Seeding data..")
-    # Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Apple", 5], pool: DBConnection.ConnectionPool)
-    # Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Pear", 4], pool: DBConnection.ConnectionPool)
-    # Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Banana", 7], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "INSERT INTO classes(school_name) VALUES($1)", ["TE4"], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "INSERT INTO classes(school_name) VALUES($1)", ["3B"], pool: DBConnection.ConnectionPool)
   end
 
 end
