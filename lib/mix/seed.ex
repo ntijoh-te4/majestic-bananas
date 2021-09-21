3967800144cc2a015f691a50f71e32b6cd08e4c1
Andreas-V <andreas.vins@elev.ga.ntig.se>
2021-09-21 11:41:46 +0200

    CSS för spelet och student index + syntax

--- lib/mix/seed.ex
+++ lib/mix/seed.ex
@@ -47,7 +47,7 @@ defmodule Mix.Tasks.Seed do
     Postgrex.query!(DB, "INSERT INTO classes(class_name, school_id) VALUES($1, $2)", ["SY18", 2], pool: DBConnection.ConnectionPool)
 
     IO.puts("Seeding student data..")
-    Postgrex.query!(DB, "INSERT INTO students(student_name, student_url, school_id, class_id) VALUES($1, $2, $3, $4)", ["Tom Cruise", "/images/Tom.png", 1, 1], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "INSERT INTO students(student_name, student_url, school_id, class_id) VALUES($1, $2, $3, $4)", ["Tom Cruise", "/images/Tom.jpg", 1, 1], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "INSERT INTO students(student_name, student_url, school_id, class_id) VALUES($1, $2, $3, $4)", ["Chris Hemsworth", "/images/chris.jpg", 2, 2], pool: DBConnection.ConnectionPool)
 
     IO.puts("Seeding user data..")
